6e61de49c17daf8901cb09bb36478cc98226d52c
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-20 21:55:51 -0700

    Integrate dynamic correspondence requests to gpt api depending on 'corr_type'

--- app/utils/gpt.py
+++ app/utils/gpt.py
@@ -1,6 +1,18 @@
 from .prompts import (
-    cl_prompt_one, cl_prompt_two, cl_prompt_three, corr_linkedin_follow_up
-    )
+    cl_prompt_one,
+    cl_prompt_two,
+    cl_prompt_three,
+    corr_application_follow_up,
+    corr_initial_connection_request,
+    corr_application_follow_up,
+    corr_informational_interview_request,
+    corr_thank_you_informational_interview,
+    corr_thank_you_formal_interview,
+    corr_request_for_feedback,
+    corr_job_offer_follow_up,
+    corr_job_offer_acceptance_decline,
+    corr_reconnection
+)
 import os
 import openai
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -38,6 +50,7 @@ def generate_gpt_cover_letter(resume, job_description, company_details, engine):
 
     return call_gpt(messages, engine)
 
+
 def generate_gpt_correspondence(context, corr_type, engine):
     """
     Generates a correspondence using OpenAI's chat completion api.
@@ -45,8 +58,36 @@ def generate_gpt_correspondence(context, corr_type, engine):
     """
     # Determine prompt based on passed in type
     prompt = None
-    if corr_type.lower() == 'linkedin post-application follow-up':
-        prompt = corr_linkedin_follow_up(context)
+    if corr_type.lower() == 'application follow-up':
+        # Context should be a cover letter
+        prompt = corr_application_follow_up(context)
+    elif corr_type.lower() == 'initial connection':
+        # Context should be a cover letter
+        prompt = corr_initial_connection_request(context)
+    elif corr_type.lower() == 'informational interview':
+        # Context should be a cover letter
+        prompt = corr_informational_interview_request(context)
+    elif corr_type.lower() == 'thank you informational interview':
+        # Context should be a cover letter
+        prompt = corr_thank_you_informational_interview(context)
+    elif corr_type.lower() == 'thank you formal interview':
+        # Context should be a cover letter
+        prompt = corr_thank_you_formal_interview(context)
+    elif corr_type.lower() == 'request feedback':
+        # Context should be job application rejection details or in absence a cover letter, eg. rejected in the first or last round of interviews etc...
+        prompt = corr_request_for_feedback(context)
+    elif corr_type.lower() == 'job offer follow-up':
+        # Context should be a cover letter or job offer details
+        prompt = corr_job_offer_follow_up(context)
+    elif corr_type.lower() == 'job offer accept':
+        # Context should be a cover letter or job offer details
+        prompt = corr_job_offer_acceptance_decline(context)
+    elif corr_type.lower() == 'job offer decline':
+        # Context should be a cover letter or job offer details
+        prompt = corr_job_offer_acceptance_decline(context, 'decline offer')
+    elif corr_type.lower() == 'reconnection':
+        # Context should be any messages or description of previous correspondence
+        prompt = corr_reconnection(context)
 
     messages = [
         {"role": "system", "content": "You will act as an expert LinkedIn and email corresponder for job hunters. You will create LinkedIn and Email follow-up messages for recruiters and create replies to emails related to the job search."},
--- app/utils/prompts.py
+++ app/utils/prompts.py
@@ -12,6 +12,7 @@ def cl_prompt_one(resume, job_description):
     
     """
 
+
 def cl_prompt_two(company_details):
     return f"""Now using some new details about the company, and the resume I provided you with earlier, include a paragraph at the beginning of the cover letter that explains why, on a personal level I am a good fit for the company.
     Be sure to avoid overly strong adjectives and avoid sounding cliche, or trite. This new paragraph should feel real. Explain why I want to work at this company in this paragraph.
@@ -22,6 +23,7 @@ def cl_prompt_two(company_details):
 
     """
 
+
 def cl_prompt_three():
     return f"""Now, keep the interest and personal connection to the company in the next revision. This time, when you rewrite it, include toward the middle and a tasteful advertisement of my qualification match for the position, based on comparing my resume to the job description.
     
@@ -30,29 +32,22 @@ def cl_prompt_three():
 
 # ------- Correspondence Prompts -----
 # Application follow up
-def corr_linkedin_follow_up(context):
+def corr_application_follow_up(context):
     return f"""I just submitted a job application and I want to follow up with the recruiter on linked in who posted the position. Please use my submitted cover letter as context to write a very brief follow-up message with the recruiter, expressing my interest in the position.
-    Use the cover letter to inform this response, not create a duplicate. Avoid cliche's a be friendly. This should look like a miniature cover letter, hitting on the most critical points.
+    Use the cover letter to inform this response, not create a duplicate. Avoid cliche's and be friendly. This should look like a miniature cover letter, hitting on the most critical points.
     ---
     Original cover letter:
     {context}
 
     """
+
 # Initial connection request:
 def corr_initial_connection_request(context):
-    return f"""I want to connect with a recruiter on LinkedIn and introduce myself. I am interested in a specific job opening or the company they represent. Please generate a personalized connection request message based on the following context:
-    ---
+    return f"""I want to connect with a recruiter on LinkedIn and introduce myself. I am interested in a specific job opening or the company they represent. Please generate a connection request message based on the following cover letter. Avoid sounding overly syncophantic or cliche. Keep the response to 5 or 6 sentences.
     Job opening or company interest:
     {context}
     """
-# Application follow-up:
-def corr_application_follow_up(context):
-    return f"""I just submitted a job application and I want to follow up with the recruiter on LinkedIn who posted the position. Please use my submitted cover letter as context to write a very brief follow-up message with the recruiter, expressing my interest in the position.
-    Use the cover letter to inform this response, not create a duplicate. Avoid cliches and be friendly. This should look like a miniature cover letter, hitting on the most critical points.
-    ---
-    Original cover letter:
-    {context}
-    """
+
 # Informational interview request:
 def corr_informational_interview_request(context):
     return f"""I would like to request a brief informational interview with a recruiter to learn more about a specific role, company, or industry. Please generate a message to request an informational interview based on the following context:
@@ -83,31 +78,20 @@ def corr_request_for_feedback(context):
     """
 # Job offer follow-up:
 def corr_job_offer_follow_up(context):
-    return f"""I received a job offer and I want to follow up with the recruiter for clarification on details such as salary, benefits, start date, or other relevant aspects. Please generate a message to follow up on the job offer based on the following context:
+    return f"""I received a job offer and I want to follow up with the recruiter for clarification on details such as salary, benefits, start date, or other relevant aspects. Please generate a message to follow up on the job offer based on the job title mentioned in the cover letter provided:
     ---
     Job offer details:
     {context}
     """
 # Job offer acceptance or decline:
-def corr_job_offer_acceptance_decline(context):
-    return f"""I want to formally accept or decline a job offer and express gratitude for the opportunity. Please generate a message with my decision based on the following context:
+def corr_job_offer_acceptance_decline(context, decision='accept offer'):
+    return f"""I want to formally accept or decline a job offer and express gratitude for the opportunity. Please generate a message with my decision based on job title from the following cover letter. Focus on sounding genuine, but don't recite facts from the cover letter in the generated response. Keep it brief, no more than 5-6 sentences and professional:
     ---
-    Job offer decision context:
+    Job title found in this cover letter:
     {context}
-    """
-# LinkedIn recommendation request:
-def corr_linkedin_recommendation_request(context):
-    return f"""I want to request a LinkedIn recommendation from a recruiter I have had a positive experience with. Please generate a message requesting a recommendation based on the following context:
     ---
-    Positive experience with the recruiter:
-    {context}
-    """
-# Status update:
-def corr_status_update(context):
-    return f"""I want to inform a recruiter I have previously interacted with about a significant update in my professional life, such as a new job, promotion, or relevant accomplishment. Please generate a message to share the update based on the following context:
-    ---
-    Professional life update:
-    {context}
+
+    Decision: {decision}
     """
 # Reconnection:
 def corr_reconnection(context):

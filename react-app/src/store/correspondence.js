c15a2585afbb00e6cad34352ea6c9d96aa650665
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-21 15:15:54 -0700

    Add correspondence actions and thunks in redux store

--- react-app/src/store/correspondence.js
+++ react-app/src/store/correspondence.js
@@ -1,9 +1,140 @@
 // constants
+const CREATE = 'correspondence/CREATE_CORRESPONDENCE';
+const POPULATE = 'correspondence/POPULATE_USER_CORRESPONDENCES';
+const READ = 'correspondence/READ_SINGLE_CORRESPONDENCE';
+const UPDATE = 'correspondence/UPDATE_CORRESPONDENCE';
+const DELETE = 'correspondence/DELETE_CORRESPONDENCE';
 
 // -------- ACTIONS ---------
+const createCorrespondence = (correspondence) => ({
+  type: CREATE,
+  correspondence,
+});
 
+const readCorrespondences = (correspondences) => ({
+  type: POPULATE,
+  correspondences,
+});
+
+const readSingleCorrespondence = (correspondence) => ({
+  type: READ,
+  correspondence,
+});
+
+const updateCorrespondence = (correspondence) => ({
+  type: UPDATE,
+  correspondence,
+});
+
+const deleteCorrespondence = (correspondenceId) => ({
+  type: DELETE,
+  correspondenceId,
+});
 
 // ----- THUNK ACTIONS ------
+export const createCorrespondenceThunk = (applicationId, correspondence) => async (dispatch) => {
+  const response = await fetch(`/api/applications/${applicationId}/correspondences`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(correspondence),
+  });
+  if (response.ok) {
+    const newCorrespondence = await response.json();
+    await dispatch(createCorrespondence(newCorrespondence));
+    return newCorrespondence;
+  } else {
+    throw new Error('Error creating correspondence');
+  }
+};
 
+export const fetchAllCorrespondencesThunk = () => async (dispatch) => {
+  const response = await fetch('/api/correspondences/');
+  if (response.ok) {
+    const normalizedCorrespondences = {};
+    const correspondences = await response.json();
+    correspondences.forEach((correspondence) => {
+      normalizedCorrespondences[correspondence.id] = correspondence;
+    });
+    await dispatch(readCorrespondences(normalizedCorrespondences));
+    return normalizedCorrespondences;
+  } else {
+    throw new Error('Error fetching correspondences');
+  }
+};
+
+export const fetchSingleCorrespondenceThunk = (correspondenceId) => async (dispatch) => {
+  const response = await fetch(`/api/correspondences/${correspondenceId}`);
+  if (response.ok) {
+    const correspondence = await response.json();
+    await dispatch(readSingleCorrespondence(correspondence));
+    return correspondence;
+  } else {
+    throw new Error('Error fetching single correspondence');
+  }
+};
+
+export const updateCorrespondenceThunk = (correspondence) => async (dispatch) => {
+  const response = await fetch(`/api/correspondences/${correspondence.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(correspondence),
+  });
+  if (response.ok) {
+    const updatedCorrespondence = await response.json();
+    await dispatch(updateCorrespondence(updatedCorrespondence));
+    await dispatch(readSingleCorrespondence(updatedCorrespondence));
+  } else {
+    throw new Error('Error updating correspondence');
+  }
+};
+
+export const deleteCorrespondenceThunk = (correspondenceId) => async (dispatch) => {
+  const response = await fetch(`/api/correspondences/${correspondenceId}`, {
+    method: 'DELETE',
+  });
+  if (response.ok) {
+    await dispatch(deleteCorrespondence(correspondenceId));
+    return 'Successfully deleted';
+  } else {
+    throw new Error('Error deleting correspondence');
+  }
+}
 
 // -------- REDUCER ---------
+const initialState = {
+  allCorrespondences: {},
+  currentCorrespondence: {},
+};
+  
+export default function correspondencesReducer(state = initialState, action) {
+  const newState = { ...state };
+  switch (action.type) {
+    case POPULATE:
+      newState.allCorrespondences = { ...action.correspondences };
+      return newState;
+    case READ:
+      newState.currentCorrespondence = { ...action.correspondence };
+      return newState;
+    case CREATE:
+      newState.currentCorrespondence = { ...action.correspondence };
+      newState.allCorrespondences = {
+      ...state.allCorrespondences,
+      ...action.correspondence,
+      };
+      return newState;
+    case UPDATE:
+      newState.allCorrespondences = { ...state.allCorrespondences };
+      newState.allCorrespondences[action.correspondence.id] = action.correspondence;
+      return newState;
+    case DELETE:
+      newState.allCorrespondences = { ...state.allCorrespondences };
+      delete newState.allCorrespondences[action.correspondenceId];
+      return newState;
+    default:
+      return state;
+  }
+  }

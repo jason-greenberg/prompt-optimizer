0b087b949f6140a9b2fcb92abdb77c7b55c32fb6
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-22 16:15:48 -0700

    Add styling

--- react-app/src/components/Resumes/CreateResume/CreateResumeForm.js
+++ react-app/src/components/Resumes/CreateResume/CreateResumeForm.js
@@ -1,12 +1,23 @@
+import { useEffect } from 'react'
 import Navigation from '../../Navigation'
+import { useMenuSelector } from '../../../context/Menu'
 import './CreateResume.css'
 
 export default function CreateResumeForm() {
+  const { setSelectedLink } = useMenuSelector()
+
+  useEffect(() => {
+    setSelectedLink('resumes')
+  }, [])
+
   return (
     <>
       <Navigation />
       <div className="create-resume-page-container">
-        <h1>Create new Resume</h1>
+        <div className="create-resume-body">
+          <h1>Upload a new Resume</h1>
+          <div>Paste your resume below</div>
+        </div>
       </div>
     </>
   )

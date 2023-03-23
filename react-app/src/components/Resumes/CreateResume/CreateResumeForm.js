e4a31a570ea220ec7a9800c3acab0c8e01d53ab5
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-23 10:06:08 -0700

    Add styling CreateResumeForm

--- react-app/src/components/Resumes/CreateResume/CreateResumeForm.js
+++ react-app/src/components/Resumes/CreateResume/CreateResumeForm.js
@@ -12,7 +12,7 @@ export default function CreateResumeForm() {
   const { setSelectedLink } = useMenuSelector();
   const [resumeText, setResumeText] = useState('');
   const [positionType, setPositionType] = useState('');
-  const [skillLevel, setSkillLevel] = useState('');
+  const [skillLevel, setSkillLevel] = useState('Entry Level');
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
@@ -55,11 +55,10 @@ export default function CreateResumeForm() {
         <div className="create-resume-body">
           <h1><span className="form-action">Upload a new</span> <span className="form-title">Resume</span></h1>
           <div>Paste your resume below</div>
-          <div className={`resume-text-box ${errors.resumeText ? 'error' : ''}`}>
-            <div>Paste resume</div>
+          <div className={`resume-input-box ${errors.resumeText ? 'error' : ''}`}>
+            <div className="input-msg">Paste resume</div>
             <textarea 
-              cols="30" 
-              rows="10"
+              placeholder='"Satya Nadella, Redmond WA..."'
               value={resumeText}
               onChange={(e) => {
                 setResumeText(e.target.value)
@@ -69,10 +68,11 @@ export default function CreateResumeForm() {
             </textarea>
             {errors.resumeText && <div className="error-message">{errors.resumeText}</div>}
           </div>
-          <div className={`position-type-box ${errors.positionType ? 'error' : ''}`}>
-            <div>Describe in 1-3 words the types of positions this resume is for</div>
+          <div className={`position-type-box resume-input-box ${errors.positionType ? 'error' : ''}`}>
+            <div className="input-msg">Describe in 1-3 words the types of positions this resume is for</div>
             <input 
               type="text" 
+              className="position-type-input"
               placeholder='"Fullstack"'
               value={positionType}
               onChange={(e) => {
@@ -82,8 +82,8 @@ export default function CreateResumeForm() {
             />
             {errors.positionType && <div className="error-message">{errors.positionType}</div>}
           </div>
-          <div className={`skill-level-box ${errors.skillLevel ? 'error' : ''}`}>
-            <div>Select Target Skill Level</div>
+          <div className={`skill-level-box resume-input-box ${errors.skillLevel ? 'error' : ''}`}>
+            <div className="input-msg">Select Target Skill Level</div>
             <select
               className="select-level-input"
               value={skillLevel}
@@ -92,7 +92,6 @@ export default function CreateResumeForm() {
                 setErrors({...errors, skillLevel: null})
               }}
             >
-              <option value="" selected disabled hidden>Select</option>
               <option value="Entry Level" key="entry">Entry Level</option>
               <option value="Mid Level" key="Mid">Mid Level</option>
               <option value="Senior Level" key="Senior">Senior Level</option>

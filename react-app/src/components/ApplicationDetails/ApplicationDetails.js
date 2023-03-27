e39981f329fa0f6fdce2c5d56c561ea769ea7cd3
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-27 13:23:50 -0700

    Add styling ApplicationDetails

--- react-app/src/components/ApplicationDetails/ApplicationDetails.js
+++ react-app/src/components/ApplicationDetails/ApplicationDetails.js
@@ -24,6 +24,7 @@ export default function ApplicationDetails() {
   const allResumesArray = Object.values(allResumes);
   const { selectedSide, setSelectedSide } = useMenuSelector();
   const [showManageDropdown, setShowManageDropdown] = useState(false)
+  const [editSelected, setEditSelected] = useState(true)
 
   useEffect(() => {
     const fetchAsync = async () => {
@@ -74,8 +75,31 @@ export default function ApplicationDetails() {
                     <img className="down" src={downArrow} alt="" />
                     { showManageDropdown && (
                       <div className="manage-app-dropdown">
-                        <div className="edit-app">Edit</div>
-                        <div className="delete-app">Delete</div>
+                        <div className="manage-selection">
+                          <div 
+                            className={`edit-app ${editSelected ? 'selected-edit' : ''}`}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              e.stopPropagation();
+                              setEditSelected(true)
+                            }}
+                          >
+                            Edit
+                          </div>
+                          <div 
+                            className={`delete-app ${!editSelected ? 'selected-delete' : ''}`}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              e.stopPropagation();
+                              setEditSelected(false)
+                            }}
+                          >
+                            Delete
+                          </div>
+                        </div>
+                        <div className="manage-option">
+                          <div>Content</div>
+                        </div>
                       </div>
                     )}
                   </div>

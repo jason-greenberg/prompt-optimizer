c7e2ca37f6d89d205873663a6f545f7a699520bf
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-28 15:11:43 -0700

    Add styling AllCorrespondences

--- react-app/src/components/Correspondences/AllCorrespondences/AllCorrespondences.js
+++ react-app/src/components/Correspondences/AllCorrespondences/AllCorrespondences.js
@@ -1,47 +1,66 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux'
-import './AllCorrespondences.css'
-import liIcon from './assets/li-blue-icon.png'
-import emailIcon from './assets/gmail.png'
+import { useSelector } from 'react-redux';
+import './AllCorrespondences.css';
+import liIcon from './assets/li-blue-icon.png';
+import emailIcon from './assets/gmail.png';
 
 export default function AllCorrespondences() {
-  const correspondences = useSelector(state => state.correspondences.currentApplicationCorrespondences);
-  const [notFound, setNotFound] = useState(false)
+  const correspondences = useSelector(
+    (state) => state.correspondences.currentApplicationCorrespondences
+  );
+  const [notFound, setNotFound] = useState(false);
+
+  // Add a new state to store the expanded status of responses
+  const [expanded, setExpanded] = useState({});
+
+  const handleClick = (index) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [index]: !prevExpanded[index],
+    }));
+  };
+
   return (
     <>
-      { !notFound && (
+      {!notFound && (
         <div className="job-description-container">
           <div className="corr-container">
-            { Object.values(correspondences).map((corr, index) => (
-              <div className="corr-w-break">
-                <div className="individual-corr" key={index}>
+            {Object.values(correspondences).map((corr, index) => (
+              <div className="corr-w-break" key={index}>
+                <div className="individual-corr">
                   <div className="corr-left">
-                    { corr.corr_type === 'LinkedIn' && (
+                    {corr.corr_type === 'LinkedIn' && (
                       <img src={liIcon} alt="linkedIn-icon" />
-                    ) }
-                    { corr.corr_type === 'Email' && (
+                    )}
+                    {corr.corr_type === 'Email' && (
                       <img src={emailIcon} alt="email-icon" />
                     )}
                   </div>
                   <div className="corr-right">
-                    <div className="corr-type">
+                    <div className="corr-type" onClick={() => handleClick(index)}>
                       <div className="corr-type-word">{corr.corr_type}:</div>
-                      <div className="response">{corr.generated_response}</div>
+                      <div
+                        className={`response ${expanded[index] ? 'expanded' : ''}`}
+                      >
+                        {corr.generated_response}
+                      </div>
                     </div>
                     <div className="corr-date">{corr.created_at}</div>
                   </div>
                 </div>
-                <div className="break"></div>
+                { index < Object.values(correspondences).length - 1 && (
+                  <div className="break"></div>
+                )}
               </div>
             ))}
           </div>
         </div>
       )}
-      { notFound && (
+      {notFound && (
         <div className="not-found">
           <h1>Job Description Not Found</h1>
         </div>
       )}
     </>
-  )
+  );
 }

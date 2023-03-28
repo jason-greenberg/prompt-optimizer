865accae709475288e89ca310b1b18b6a49442b8
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-28 16:05:42 -0700

    Connect corresponce thunks to Correspondence dropdown

--- react-app/src/components/Correspondences/CorrespondenceDropdown/CorrespondenceDropdown.js
+++ react-app/src/components/Correspondences/CorrespondenceDropdown/CorrespondenceDropdown.js
@@ -1,38 +1,58 @@
-import { useSelector } from 'react-redux'
-import './CorrespondenceDropdown.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { createCorrespondenceThunk } from '../../../store/correspondence';
+import './CorrespondenceDropdown.css';
 
 export default function CorrespondenceDropdown() {
-  const application = useSelector(state => state.applications.currentApplication)
+  const dispatch = useDispatch();
+  const application = useSelector((state) => state.applications.currentApplication);
+  const coverLetter = useSelector((state) => state.coverletters?.currentCoverLetter);
+  const { applicationId } = useParams();
+
+  const handleCorrespondence = async (e, corr_type) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const response = await dispatch(
+      createCorrespondenceThunk(applicationId, {
+        corr_type: corr_type,
+        context: coverLetter.letter_text,
+        engine: 'gpt-3.5-turbo',
+      }),
+    );
+  };
 
   return (
     <div className="corr-drop-container">
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'application follow-up')}>
         Application follow-up
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'initial connection')}>
         Initial connection request
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'informational interview')}>
         Informational interview request
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'thank you informational interview')}>
         Thank you after informational interview
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'thank you formal interview')}>
         Thank you after formal job interview
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'request feedback')}>
         Request for feedback after rejection
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'job offer follow-up')}>
         Job offer follow-up
       </div>
-      <div className="corr-option">
-        Job offer acceptance or decline
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'job offer accept')}>
+        Job offer acceptance
       </div>
-      <div className="corr-option">
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'job offer decline')}>
+        Job offer decline
+      </div>
+      <div className="corr-option" onClick={(e) => handleCorrespondence(e, 'reconnection')}>
         Reconnection
       </div>
     </div>
-  )
+  );
 }

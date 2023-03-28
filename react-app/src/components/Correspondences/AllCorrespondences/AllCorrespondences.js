37412be9d5f53614c438c472db3d028f79d04aff
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-28 14:49:33 -0700

    Build out AllCorrespondences component

--- react-app/src/components/Correspondences/AllCorrespondences/AllCorrespondences.js
+++ react-app/src/components/Correspondences/AllCorrespondences/AllCorrespondences.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux'
 import './AllCorrespondences.css'
+import liIcon from './assets/li-blue-icon.png'
+import emailIcon from './assets/gmail.png'
 
 export default function AllCorrespondences() {
   const correspondences = useSelector(state => state.correspondences.currentApplicationCorrespondences);
@@ -9,15 +11,27 @@ export default function AllCorrespondences() {
     <>
       { !notFound && (
         <div className="job-description-container">
-          <div className="job-text">
+          <div className="corr-container">
             { Object.values(correspondences).map((corr, index) => (
-              <div className="individual-corr" key={index}>
-                <div>logo</div>
-                <div className="corr-type">
-                  <div>{corr.corr_type}</div>
-                  <div>{corr.generated_response}</div>
+              <div className="corr-w-break">
+                <div className="individual-corr" key={index}>
+                  <div className="corr-left">
+                    { corr.corr_type === 'LinkedIn' && (
+                      <img src={liIcon} alt="linkedIn-icon" />
+                    ) }
+                    { corr.corr_type === 'Email' && (
+                      <img src={emailIcon} alt="email-icon" />
+                    )}
+                  </div>
+                  <div className="corr-right">
+                    <div className="corr-type">
+                      <div className="corr-type-word">{corr.corr_type}:</div>
+                      <div className="response">{corr.generated_response}</div>
+                    </div>
+                    <div className="corr-date">{corr.created_at}</div>
+                  </div>
                 </div>
-                <div className="corr-date">{corr.created_at}</div>
+                <div className="break"></div>
               </div>
             ))}
           </div>

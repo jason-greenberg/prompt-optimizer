c4c23c1bc44746279f7f24d7289b9946a0b54974
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-30 13:45:40 -0700

    Add animation to loading messages

--- react-app/src/components/Loading/LoadingDefault.js
+++ react-app/src/components/Loading/LoadingDefault.js
@@ -1,35 +1,29 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from '../Navigation';
 import './LoadingDefault.css';
 import loadingGif from './assets/loading-bars.gif';
 import { loadingMessages } from '../../utils/loading-messages';
 
 export default function LoadingDefault() {
-  const [currentMessage, setCurrentMessage] = useState('');
-
   const getRandomArrayValue = (array) => {
     const index = Math.floor(Math.random() * array.length);
     return array[index];
   };
 
+  const [message, setMessage] = useState(getRandomArrayValue(loadingMessages));
+
   useEffect(() => {
-    const updateMessage = () => {
-      setCurrentMessage(getRandomArrayValue(loadingMessages));
-    };
-
-    updateMessage(); // Set initial message
-    const interval = setInterval(updateMessage, 5000); // Update message every 5 seconds
-
-    return () => {
-      clearInterval(interval); // Clear the interval when the component unmounts
-    };
+    const interval = setInterval(() => {
+      setMessage(getRandomArrayValue(loadingMessages));
+    }, 5000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <>
       <div className="loading-page-container">
         <div className="loading-page-body">
-          <h1>{currentMessage}</h1>
+          <h1 className="loading-message">{message}</h1>
           <img className="loading-gif" src={loadingGif} alt="loading-gif" />
         </div>
       </div>

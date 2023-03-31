398eade8a479e3644b35ec9beafa5a4c36818b25
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-31 16:33:24 -0700

    Add stripe to dependencies

---
+++ app/api/payment_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify, make_response, request
+from flask_login import login_required, current_user
+from app.models import db
+
+payment_routes = Blueprint('payments', __name__)
+
+# Create checkout session
+@payment_routes.route('/create', methods=['POST'])
+@login_required
+def create_checkout_session():
+    """
+    Create a new checkout session with stripe
+    """

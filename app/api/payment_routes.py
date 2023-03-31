e6237fc1312b623b85a774390ec64cb96ef05ba7
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-31 16:53:56 -0700

    Create checkout session route payments

--- app/api/payment_routes.py
+++ app/api/payment_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, redirect
 from flask_login import login_required, current_user
 from app.models import db
 import stripe
@@ -8,6 +8,8 @@ payment_routes = Blueprint('payments', __name__)
 
 stripe.api_key = os.getenv('STRIPE_API_KEY')
 
+YOUR_DOMAIN = 'http://localhost:8000'
+
 # Create checkout session
 @payment_routes.route('/create', methods=['POST'])
 @login_required
@@ -15,4 +17,19 @@ def create_checkout_session():
     """
     Create a new checkout session with stripe
     """
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items = [
+                { 
+                    'price': 'price_1MrqDSAshXPLTdi40vKdNhfZ',
+                    'quantity': 1
+                }
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success',
+            cancel_url=YOUR_DOMAIN + '/cancel'
+        )
+    except Exception as e:
+        return str(e)
     
+    return { 'message': 'success' }

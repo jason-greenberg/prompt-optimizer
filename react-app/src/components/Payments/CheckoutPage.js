63c97f0ce0c96a67c26fa348649ff294f4ed945e
jason-greenberg <jasongreenberg07@gmail.com>
2023-04-03 14:36:14 -0700

    Add styling CheckoutPage

--- react-app/src/components/Payments/CheckoutPage.js
+++ react-app/src/components/Payments/CheckoutPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import './CheckoutPage.css'
 import Navigation from '../Navigation';
@@ -20,38 +20,61 @@ async function redirectToCheckout() {
 
 export default function CheckoutPage() {
 
+  const [selectedPackage, setSelectedPackage] = useState(null);
+
   const checkoutObj = {
     1: { id: 1, 'package': 'Basic Package', calls: 20, price: '$0.99' },
     2: { id: 2, 'package': 'Pro Package', calls: 100, price: '$3.99' },
     3: { id: 3, 'package': 'Elite Package', calls: 500, price: '$9.99' },
   }
 
+  const handlePackageClick = (planId) => {
+    setSelectedPackage(planId);
+  }
+
   return (
     <>
-        <Navigation />
-        <div className="checkout-page-container">
-            <div className="checkout-page-body">
-                <h4 className="checkout-table-title">Pick a Package</h4>
-                <div className="packages-grid">
-                    <div className="header-container">
-                        <div className="col-name package-title">Package Type</div>
-                        <div className="col-name">AI Generations</div>
-                        <div className="col-name">Price</div>
-                    </div>
-                    <div className="applications-container">
-                        {Object.values(checkoutObj).map((plan) => (
-                            <div key={plan.id} className="indiv-package">
-                                <div className="package-title col-detail">{plan.package}</div>
-                                <div className="col-detail">{plan.calls}</div>
-                                <div className="col-detail">{plan.price}</div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <button onClick={redirectToCheckout}>Checkout</button>
+      <Navigation />
+      <div className="checkout-page-container">
+        <div className="checkout-page-body">
+          <h4 className="checkout-table-title">Pick a Package</h4>
+          <div className="packages-grid">
+            <div className="header-container">
+              <div className="col-name package-title">Package Type</div>
+              <div className="col-name">AI Generations</div>
+              <div className="col-name">Price</div>
             </div>
+            <div className="applications-container">
+              {Object.values(checkoutObj).map((plan) => (
+                <div
+                  key={plan.id}
+                  className={"indiv-package" + (selectedPackage === plan.id ? ' selected-package' : '')}
+                  onClick={() => handlePackageClick(plan.id)}
+                >
+                  <div className="checkbox">
+                    <input
+                      type="radio"
+                      checked={selectedPackage === plan.id}
+                      readOnly
+                    />
+                  </div>
+                  <div className="package-title col-detail">{plan.package}</div>
+                  <div className="col-detail">
+                    {plan.calls} <span className="gens">gens</span>
+                  </div>
+                  <div className="col-detail">{plan.price}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button
+            onClick={redirectToCheckout}
+            className="create-button checkout-button"
+          >
+            Checkout
+          </button>
         </div>
+      </div>
     </>
-);
-
+  );
 }

35e2433143a7c793e5f44a87123ad938fe528c64
jason-greenberg <jasongreenberg07@gmail.com>
2023-04-04 08:40:53 -0700

    Add generation_balance column to Users table

--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    generation_balance = db.Column(db.Integer, default=0, nullable=False)
 
     applications = db.relationship('Application', back_populates='user')
     cover_letters = db.relationship('CoverLetter', back_populates='user')
--- migrations/versions/690a1ef4ad49_.py
+++ migrations/versions/690a1ef4ad49_.py
@@ -26,6 +26,7 @@ def upgrade():
     sa.Column('username', sa.String(length=40), nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
     sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    sa.Column('generation_balance', sa.Integer(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')

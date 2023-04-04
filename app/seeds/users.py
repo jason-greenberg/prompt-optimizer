83136dafdff971ef4154d7d84f340a27b76d1358
jason-greenberg <jasongreenberg07@gmail.com>
2023-04-04 09:31:58 -0700

    Update users table seeder file to reflect new column

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,11 @@ from sqlalchemy.sql import text
 # Adds a demo user
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', generation_balance=10)
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', generation_balance=5)
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', generation_balance=5)
 
     db.session.add(demo)
     db.session.add(marnie)

85df24d277d4b8b5a613c6195f954b718024a272
jason-greenberg <jasongreenberg07@gmail.com>
2023-04-07 16:23:44 -0700

    Add typing svg to splash page

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,10 +1,32 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import './SplashPage.css';
 
 export default function SplashPage() {
   const user = useSelector(state => state.session.user)
+  const svgUrls = [
+    'https://readme-typing-svg.demolab.com?font=Oxygen&weight=700&size=62&duration=800&pause=800&color=000000&repeat=true&multiline=true&width=700&height=100&lines=write+a+cover+letter.',
+    'https://readme-typing-svg.demolab.com?font=Oxygen&weight=700&size=62&duration=800&pause=800&color=000000&repeat=true&multiline=true&width=700&height=100&lines=network+with+recruiters.',
+    'https://readme-typing-svg.demolab.com?font=Oxygen&weight=700&size=62&duration=800&pause=8000&color=000000&repeat=true&multiline=true&width=700&height=100&lines=turn+in+your+job+app.',
+    'https://readme-typing-svg.demolab.com?font=Oxygen&weight=700&size=62&duration=800&pause=800&color=000000&repeat=true&multiline=true&width=700&height=100&lines=track+your+job+apps.',
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const cycle = () => {
+      setCurrentIndex((currentIndex + 1) % svgUrls.length);
+    };
+
+    const interval = setInterval(cycle, 1600); // Adjust the duration based on the total time it takes for an SVG to finish typing
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentIndex]);
+
+  const typingSvg = <img src={svgUrls[currentIndex]} alt="Typing SVG" />;
 
   // add no-scrolling styling to splash page only
   useEffect(() => {
@@ -35,7 +57,9 @@ export default function SplashPage() {
       </div>
       <div className="splash-body-container">
         <div className="message-container">
-          <div className="message">The fastest way to write a cover letter.</div>
+          <div className="message">
+            The fastest way to {typingSvg}
+          </div>
         </div>
       </div>
     </div>

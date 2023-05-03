5d5dafd487666a58a24003fdbf54ea807c16b6ff
jason-greenberg <jasongreenberg07@gmail.com>
2023-05-03 12:50:38 -0700

    Connect apply link to application, jobDetails component

--- react-app/src/components/ApplicationDetails/JobDetails.js
+++ react-app/src/components/ApplicationDetails/JobDetails.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
 import './JobDetails.css';
+import { getJobBoardFromUrl } from "../../utils/format";
 
 export default function JobDetails() {
   const application = useSelector(state => state.applications.currentApplication)
@@ -12,8 +13,11 @@ export default function JobDetails() {
       { !notFound && (
         <div className="job-description-container">
           { application.apply_link && (
-            <button className="view-button apply-direct-button">
-              {`Apply directly on`}
+            <button 
+              className="view-button apply-direct-button"
+              onClick={}
+            >
+              {`Apply directly on ${getJobBoardFromUrl(application.apply_link)}`}
             </button>
           )}
           <div className="job-description-body">

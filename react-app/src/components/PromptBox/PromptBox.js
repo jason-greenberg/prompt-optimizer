8caeae31361f01455e442cc17a6cf7427f4deee2
jason-greenberg <jasongreenberg07@gmail.com>
2023-07-10 12:26:03 -0700

    Connect PromptBox component to createprompt thunk

--- react-app/src/components/PromptBox/PromptBox.js
+++ react-app/src/components/PromptBox/PromptBox.js
@@ -2,22 +2,26 @@ import "./PromptBox.css";
 import { useEffect, useState } from 'react';
 import Navigation from "../Navigation";
 import { useMenuSelector } from "../../context/Menu";
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { createResumeThunk } from "../../store/resume";
 import { useHistory } from 'react-router-dom'
+import { createPromptThunk, fetchAllPromptsThunk } from "../../store/prompt";
 
 export default function PromptBox() {
   const dispatch = useDispatch();
   const history = useHistory();
   const { setSelectedLink } = useMenuSelector();
-  const [resumeText, setResumeText] = useState('');
   const [prompt, setPrompt] = useState('');
-  const [skillLevel, setSkillLevel] = useState('Entry Level');
+  const [optimizedPrompt, setOptimizedPrompt] = useState('...')
   const [errors, setErrors] = useState({});
+  const gptPrompts = Object.values(useSelector(state => state.prompts.allPrompts));
 
   useEffect(() => {
-    setSelectedLink('resumes')
+    dispatch(fetchAllPromptsThunk())
   }, [])
+
+  useEffect(() => {
+  }, [gptPrompts.length])
 
   const validate = () => {
     const validationErrors = {}
@@ -32,16 +36,13 @@ export default function PromptBox() {
     const validationErrors = validate()
     setErrors(validationErrors)
 
-    // If no validation errors, submit resume
+    // If no validation errors, submit prompt
     if (!Object.keys(validationErrors).length > 0) {
-      const newResume = {
-        resume_text: resumeText,
-        position_type: prompt,
-        skill_level: skillLevel
+      const newPrompt = {
+        prompt: prompt
       }
 
-      const resume = await dispatch(createResumeThunk(newResume));
-      history.push(`/resumes/${resume.id}`)
+      await dispatch(createPromptThunk(newPrompt));
     }
   }
 
@@ -67,7 +68,7 @@ export default function PromptBox() {
           </div>
           <div className={`skill-box resume-input-box ${errors.skillLevel ? 'error' : ''}`}>
             <div className="input-msg">Our Prompt</div>
-            <div>...</div>
+            <div>{gptPrompts && gptPrompts[gptPrompts.length - 1]?.prompt}</div>
             {errors.skillLevel && <div className="error-message">{errors.skillLevel}</div>}
           </div>
           <div className="submit-container">

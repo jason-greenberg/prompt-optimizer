afbcb1e1a0811a0cfa463ffd468501a917c494e0
jason-greenberg <jasongreenberg07@gmail.com>
2023-07-11 15:25:52 -0700

    Create copy component

---
+++ react-app/src/components/CopyPaste/CopyPaste.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { copyToClipboardFormatted } from "../../utils/clipboard";
+import { formatPromptForCopy } from "../../utils/format";
+import copyIcon from "../CoverLetters/CoverLetterDetails/assets/copy-icon-grey.png"
+
+export default function CopyPaste({ textToCopy }) {
+  const [copySelected, setCopySelected] = useState(false);
+  
+  return (
+    <img
+      src={copyIcon}
+      alt="Copy"
+      className="copy-icon copy-cover"
+      onClick={(e) => {
+        setCopySelected(true)
+        e.stopPropagation();
+        copyToClipboardFormatted((formatPromptForCopy(textToCopy)));
+      }}
+    />
+  )
+}
